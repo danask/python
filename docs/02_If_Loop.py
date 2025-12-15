f6bbd7941efcefa0c3c6a4285f9bf42bed0d10cb
Daniel Ahn <danask2008@gmail.com>
2025-12-15 13:56:29 +0900

    clean up

--- docs/02_If_Loop.py
+++ docs/02_If_Loop.py
@@ -1,4 +1,15 @@
 #########################################
+
+print ("========== Loop ==============")
+
+for i in range(0, 3):  # range first
+    print(list[i])  # array next
+
+count = 0
+for val in list:
+    print(count, ":", val)
+    count += 1
+
 print ("============ if ==============")
 
 weather = "Sunny"
